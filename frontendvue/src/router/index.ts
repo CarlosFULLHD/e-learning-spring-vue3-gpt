b598b579d9601160c56b1242bc0debd2993d453c
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2023-12-01 15:28:22 -0400

    Update index.ts

--- frontendvue/src/router/index.ts
+++ frontendvue/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 // @ts-ignore
 import Homepage from '../views/Homepage.vue'
 import Otravista from '../views/Otravista.vue'
+import SubjectsView from '../views/SubjectsView.vue';
+import SubjectWelcomeView from "../views/SubjectWelcomeView.vue";
 // @ts-ignore
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,15 @@ const router = createRouter({
       path: '/otravista',
       name: 'otravista',
       component: Otravista
+    },
+    { path: '/subjects',
+      name:'subjects',
+      component: SubjectsView
+    },
+    {
+      path:'/subjectwelcome',
+      name:'subjectwelcome',
+      component: SubjectWelcomeView
     }
   ]
 })
