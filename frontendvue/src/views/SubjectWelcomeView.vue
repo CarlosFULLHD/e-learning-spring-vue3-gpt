3af169267bbd5fdc7ed691834ceb0ea3609dddc7
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2023-12-01 15:16:10 -0400

    Add files via upload

---
+++ frontendvue/src/views/SubjectWelcomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container mx-auto text-center p-6">
+    <h1 class="text-4xl font-bold mb-4">Bienvenido al Curso</h1>
+    <p class="text-md mb-8">Aquí va el por qué este curso es importante...</p>
+    <div class="flex justify-center items-center mb-8">
+      <h2 class="text-2xl font-semibold mr-4">Progreso:</h2>
+      <img src="https://media.licdn.com/dms/image/D4E12AQF_ADYai3zlzQ/article-cover_image-shrink_720_1280/0/1695160618049?e=2147483647&v=beta&t=SzZ8MZollUnUN_nv1bzXxNTbbHsIMeKm16uXqIcPSnU" alt="Progreso del Curso" class="max-w-xs md:max-w-sm lg:max-w-md">
+    </div>
+    <router-link to="/subjectwelcome" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Entrar
+    </router-link>
+  </div>
+</template>
+<style scoped>
+</style>
---
+++ frontendvue/src/views/SubjectsView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container mx-auto mt-8 mr-4 justify-center">
+    <div class="flex mb-4">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mr-4">
+        <img class="w-full" src="https://www.eleconomista.com.mx/__export/1621649541291/sites/eleconomista/img/2021/05/21/habilidades-blandas-desde-educacion-universidades.jpg_185051853.jpg" alt="Imagen de ejemplo">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Comunicación Efectiva</div>
+          <p class="text-gray-700 text-base">
+            Desarrolla habilidades para comunicarte de manera clara y efectiva
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <router-link to="/subjectwelcome" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Entrar
+          </router-link>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mr-4">
+        <img class="w-full" src="https://img.itmastersmag.com/wp-content/uploads/2023/09/29194843/habilidades-blandas-1280x720.jpg.webp" alt="Imagen de ejemplo">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Inteligencia Emocional</div>
+          <p class="text-gray-700 text-base">
+            Descubre el poder de la Inteligencia Emocional
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <router-link to="/subjectwelcome" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Entrar
+          </router-link>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="https://blog.pearsonlatam.com/hs-fs/hubfs/Blog%20HED/Multimedia/Im%C3%A1genes%20de%20Blog/soft-skills-en-la-educacion-superior-concepto.jpg?width=1200&name=soft-skills-en-la-educacion-superior-concepto.jpg" alt="Imagen de ejemplo">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Pensamiento Crítico y Resolución de Problemas</div>
+          <p class="text-gray-700 text-base">
+            Desarrolla tu capacidad de análisis y resolución de problemas complejos.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <router-link to="/subjectwelcome" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Entrar
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
